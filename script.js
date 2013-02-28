0d263e46ed9a0c83313cd9e2a81056459b5bf0af
Tianyu <noterrain@gmail.com>
2013-02-28 14:06:32 -0500

    Revert "Revert "chnage""
    This reverts commit f576f9f5bb86c41a843c3ff6e89ae3912ade22da.

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('#box1').mouseenter(function(){
+        //$item.fadeOut(1000);
+       $(this).animate({
+           height: '+=10px'
+       });   
+    
+    });
+});
+
+
+
+
